6780a341200d1c346dceeb44f17cc9ab1526650c
Loïc Dutrieux <loic.dutrieux@gmail.com>
2018-12-13 00:07:09 +0100

    working on landsat espa indexing script

--- landsat_espa.py
+++ landsat_espa.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from glob import glob
+import re
+import xml.etree.ElementTree as ET
+
+from rasterio.crs import CRS
+from pyproj import Proj
+
 def parse_scene(path):
     """Function to parse a single scene
 
@@ -7,20 +18,64 @@ def parse_scene(path):
     Return:
         dict: Item dict compliant with stac specifications
     """
+    pattern = re.compile(r'[A-Z0-9]{4}_[A-Z0-9]{4}_\d{6}_\d{8}_\d{8}_01_(T1|T2|RT)\.xml')
+    # Check that path is a dir and contains appropriate files
+    if not os.path.isdir(path):
+        raise ValueError('Argument path= is not a directory')
+    mtl_file_list = glob(os.path.join(path, '*.xml'))
+    # Filter list of xml files with regex (there could be more than one in case
+    # some bands have been opend in qgis for example)
+    mtl_file_list = [x for x in mtl_file_list if pattern.search(x)]
+    print(mtl_file_list)
+    if len(mtl_file_list) != 1:
+        raise ValueError('Could not identify a unique xml metadata file')
+    mtl_file = mtl_file_list[0]
+    # Start parsing xml
+    root = ET.parse(mtl_file).getroot()
+    ns = 'http://espa.cr.usgs.gov/v2'
+    # Build datetime from date and time
+    date_str = root.find('ns:global_metadata/ns:acquisition_date',
+                         namespaces={'ns': ns}).text
+    time_str = root.find('ns:global_metadata/ns:scene_center_time',
+                         namespaces={'ns': ns}).text
+    dt_str = '%sT%s' % (date_str, time_str[:8])
+    # satellite sensor metadata
+    sensor = root.find('ns:global_metadata/ns:instrument',
+                           namespaces={'ns': ns}).text
+    spacecraft = root.find('ns:global_metadata/ns:satellite',
+                          namespaces={'ns': ns}).text
+    # Scene corners in projected coordinates
+    ulx = float(root.find('ns:global_metadata/ns:projection_information/ns:corner_point[@location="UL"]',
+                          namespaces={'ns': ns}).attrib['x'])
+    uly = float(root.find('ns:global_metadata/ns:projection_information/ns:corner_point[@location="UL"]',
+                          namespaces={'ns': ns}).attrib['y'])
+    lrx = float(root.find('ns:global_metadata/ns:projection_information/ns:corner_point[@location="LR"]',
+                          namespaces={'ns': ns}).attrib['x'])
+    lry = float(root.find('ns:global_metadata/ns:projection_information/ns:corner_point[@location="LR"]',
+                          namespaces={'ns': ns}).attrib['y'])
+    utm_zone = int(root.find('ns:global_metadata/ns:projection_information/ns:utm_proj_params/ns:zone_code',
+                             namespaces={'ns': ns}).text)
+    crs = CRS({'proj': 'utm',
+               'zone': utm_zone})
+    # Get coorner coordinates in long lat by transforming from projected values 
+    p = Proj(crs)
     d = {"id": scene_id,
          "type": "Feature",
          "bbox": bbox,
          "geometry": geom,
          "properties": {
            "datetime": dt_str,
-           "c:level": level, # e.g. L1T
            "c:description": "Landsat data processed to surface reflectance on the espa platform",
+           "eo:processing_level": level, # e.g. L1T
+           "eo:spacecraft": spacecraft,
+           "eo:sensor": sensor,
            "eo:gsd": 30.0,
-           "eo:cloud_cover" : cc,
+           "eo:cloud_cover": cc,
+           "eo:crs": crs,
            "landsat:wrs_path": wrs_path,
            "landsat:wrs_row": wrs_row,
          },
-           "assets" :{
+         "assets" :{
            "metadata": {
              "href": metadata_path,
              "type": "mtl"
@@ -47,6 +102,10 @@ def parse_scene(path):
            },
            "swir2": {
              "href": swir2_path,
+             "type": "image/vnd.stac.geotiff",
+           },
+           "pixel_qa": {
+             "href": pixel_qa_path,
              "type": "image/vnd.stac.geotiff",
            }
          }
