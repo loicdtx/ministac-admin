b1d58a0cf716410ca033f7c7f49c933dbde38ce0
Loïc Dutrieux <loic.dutrieux@gmail.com>
2018-12-29 14:45:01 +0100

    script to add collection to the database

---
+++ add_collection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+
+import yaml
+
+import ministac
+
+
+if __name__ == '__main__':
+    epilog = """
+Read a collection description from a yaml file and add it to the ministac database
+
+--------------
+Example usage
+--------------
+./add_collection.py collections/landsat_sr_8.yaml
+"""
+    parser = argparse.ArgumentParser(epilog=epilog,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('meta_file',
+                        help='Path to yaml file containing collection metadata')
+    parsed_args = vars(parser.parse_args())
+
+    with open(parsed_args['meta_file']) as src:
+        collection = yaml.load(src)
+
+    ministac.add_collection(collection)
