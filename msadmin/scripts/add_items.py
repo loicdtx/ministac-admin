9bb8eec7d2d7a2cf88fd589d633a52f459be9ff0
Loïc Dutrieux <loic.dutrieux@gmail.com>
2019-09-17 19:38:04 +0100

    add_items.py script should now be s3 compatible

--- msadmin/scripts/add_items.py
+++ msadmin/scripts/add_items.py
@@ -5,13 +5,61 @@ import os
 import sys
 import argparse
 from importlib import import_module
+from glob import glob
+import re
 
 from jsonschema import validate
 import ministac
 from ministac.globals import ITEM_SCHEMA
 from ministac.db import session_scope
 
-from msadmin.datasets.landsat import parse
+from msadmin.utils import s3
+
+
+def main(path, pattern, bucket, collection, parser, dryrun, reason):
+    try:
+        module = import_module('msadmin.datasets.%s' % parser)
+        parse = module.parse
+    except ImportError as e:
+        raise ValueError('Invalid parser name')
+    # Build list of paths for either filesystem or s3
+    if bucket is None:
+        # filesystem, either folders or archives
+        paths = glob(os.path.join(path, '*'))
+        paths = [x for x in paths if re.match(pattern, x)]
+    else:
+        # s3 case
+        paths = s3.list_folders(bucket=bucket, path=path, pattern=pattern)
+    if dryrun:
+        # Just a simulation to check that metadata are properly parsed
+        for path in paths:
+            print(path)
+            try:
+                scene_meta = parse(path)
+                try:
+                    validate(scene_meta, ITEM_SCHEMA)
+                    print('OK')
+                except Exception as e:
+                    print('Invalid json')
+                    if reason:
+                        # For debugging via cli when it doesn't work
+                        print(e)
+            except Exception as e:
+                print('Failed to parse scene metadata')
+                if reason:
+                    print(e)
+    else:
+        if collection is None:
+            print('You must provide the name of an existing collection')
+            sys.exit()
+        dict_list = []
+        for path in paths:
+            try:
+                dict_list.append(parse(path))
+            except Exception as e:
+                print('skipped %s\nreason: %s' % (path, e))
+        with session_scope() as session:
+            ministac.add_items(session, dict_list, collection)
 
 
 if __name__ == '__main__':
@@ -22,20 +70,29 @@ Populate the ministac database with metadata parsed from a list of scenes
 Example usage
 --------------
 # First, dry run to make sure parsing is successful
-add_items.py /path/to/scenes/LC08* --collection landsat_sr_8 --parser landsat --dryrun
+add_items.py /path/to/scenes --pattern \"LC08.*\" --collection landsat_sr_8 --parser landsat --dryrun
 
 # In case of validation errors (Invalid json), investigate what's invalid
-landsat_espa.py /path/to/scenes/LC08* --collection landsat_sr_8 --parser landsat --dryrun --reason
+# Note that pattern is optional if all subdirectories or archives present in path are to be ingested
+landsat_espa.py /path/to/scenes --collection landsat_sr_8 --parser landsat --dryrun --reason
 
 # If everything is OK, ingest the metadata in the ministac database
-landsat_espa.py /path/to/scenes/LC08* --collection landsat_sr_8 --parser landsat --dryrun
+landsat_espa.py /path/to/scenes --collection landsat_sr_8 --parser landsat --dryrun
 """
     parser = argparse.ArgumentParser(epilog=epilog,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('paths',
-                        nargs='+',
+    parser.add_argument('path',
+                        type=str,
                         help='List of directories containing landsat data processed by espa')
+    parser.add_argument('-pattern', '--pattern',
+                        type=str,
+                        default=r'.*',
+                        help='Regex pattern to filter elements contained in path')
+    parser.add_argument('-bucket', '--bucket',
+                        type=str,
+                        default=None,
+                        help='Optional name of s3 bucket')
     parser.add_argument('-c', '--collection',
                         required=False,
                         type=str,
@@ -50,38 +107,6 @@ landsat_espa.py /path/to/scenes/LC08* --collection landsat_sr_8 --parser landsat
     parser.add_argument('--reason',
                         action='store_true',
                         help='In case dry run returns validation errors (invalid jsons), re-run it and print the reason of the invalidity')
-    parsed_args = vars(parser.parse_args())
-    try:
-        module = import_module('msadmin.datasets.%s' % parsed_args['parser'])
-        parse = module.parse
-    except ImportError as e:
-        raise ValueError('Invalid parser name')
-    if parsed_args['dryrun']:
-        for path in parsed_args['paths']:
-            print(path)
-            try:
-                scene_meta = parse(path)
-                try:
-                    validate(scene_meta, ITEM_SCHEMA)
-                    print('OK')
-                except Exception as e:
-                    print('Invalid json')
-                    if parsed_args['reason']:
-                        print(e)
-            except Exception as e:
-                print('Failed to parse scene metadata')
-                if parsed_args['reason']:
-                    print(e)
-    else:
-        if parsed_args['collection'] is None:
-            print('You must provide the name of an existing collection')
-            sys.exit()
-        dict_list = []
-        for path in parsed_args['paths']:
-            try:
-                dict_list.append(parse(path))
-            except Exception as e:
-                print('skipped %s\nreason: %s' % (path, e))
-        with session_scope() as session:
-            ministac.add_items(session, dict_list, parsed_args['collection'])
+
+    main(**vars(parser.parse_args()))
 
